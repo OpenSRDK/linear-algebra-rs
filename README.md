ff52f54445b16e37ddf4eee727d1597a0a7cca79
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2020-07-07 12:05:51 +0900

    update

--- README.md
+++ README.md
@@ -12,3 +12,54 @@ openblas-src = "0.9"
 extern crate opensrdk_linear_algebra;
 extern crate openblas_src;
 ```
+
+You can also use accelerate, intel-mkl, or netlib instead.
+See [here](https://github.com/blas-lapack-rs/blas-lapack-rs.github.io/wiki).
+
+## Macro
+
+```rs
+#[cfg(test)]
+mod tests {
+    use crate::prelude::*;
+    #[test]
+    fn it_works() {
+        let a = mat!(
+            1.0, 0.0;
+            0.0, 1.0
+        );
+        assert_eq!(a[0][0], 1.0);
+        assert_eq!(a[0][1], 0.0);
+        assert_eq!(a[1][0], 0.0);
+        assert_eq!(a[1][1], 1.0);
+
+        let b = mat!(
+            &a, zeros!(2, 2);
+            zeros!(2, 2), &a
+        );
+
+        assert_eq!(b[0][0], 1.0);
+        assert_eq!(b[0][1], 0.0);
+        assert_eq!(b[3][0], 0.0);
+        assert_eq!(b[3][3], 1.0);
+    }
+}
+```
+
+```rs
+#[cfg(test)]
+mod tests {
+    use crate::prelude::*;
+    #[test]
+    fn it_works() {
+        let a = mat!(
+            1.0, 2.0;
+            3.0, 4.0
+        ) * mat!(
+            5.0, 6.0;
+            7.0, 8.0
+        );
+        assert_eq!(a[0][0], 19.0)
+    }
+}
+```
--- src/macros/zeros.rs
+++ src/macros/zeros.rs
@@ -1,13 +1,11 @@
-use crate::{number::Number, macros::sub_matrix::SubMatrix};
+use crate::{macros::sub_matrix::SubMatrix, number::Number};
 
 #[macro_export]
 macro_rules! zeros {
-    ($($e: expr),+) => {
-        {
-            use $crate::macros::zeros::Zeros;
-            Zeros($($e),+)
-        }
-    };
+    ($e1: expr, $e2: expr) => {{
+        use $crate::macros::zeros::Zeros;
+        Zeros($e1, $e2)
+    }};
 }
 
 pub struct Zeros(pub usize, pub usize);
@@ -19,5 +17,31 @@ impl<U: Number> SubMatrix<U> for Zeros {
 
     fn index(&self, _: usize, _: usize) -> U {
         U::default()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::prelude::*;
+    #[test]
+    fn it_works() {
+        let a = mat!(
+            1.0, 0.0;
+            0.0, 1.0
+        );
+        assert_eq!(a[0][0], 1.0);
+        assert_eq!(a[0][1], 0.0);
+        assert_eq!(a[1][0], 0.0);
+        assert_eq!(a[1][1], 1.0);
+
+        let b = mat!(
+            &a, zeros!(2, 2);
+            zeros!(2, 2), &a
+        );
+
+        assert_eq!(b[0][0], 1.0);
+        assert_eq!(b[0][1], 0.0);
+        assert_eq!(b[3][0], 0.0);
+        assert_eq!(b[3][3], 1.0);
     }
 }

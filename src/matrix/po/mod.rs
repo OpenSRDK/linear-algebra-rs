776c8c173e96acd3176db7aec1c27300c2147975
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2020-08-23 16:00:52 +0900

    update

--- src/matrix/po/mod.rs
+++ src/matrix/po/mod.rs
@@ -1,4 +1,5 @@
 pub mod inv;
+pub mod sv;
 pub mod trf;
 pub mod tri;
 pub mod trs;
---
+++ src/matrix/po/sv.rs
@@ -0,0 +1,31 @@
+use crate::matrix::Matrix;
+use crate::matrix::Vector;
+
+impl Matrix {
+    /// # Solve equations with Conjugate Gradient Method
+    /// for positiveDefinite matrix
+    pub fn posv_cgm(
+        vec_mul: impl Fn(&[f64]) -> Vec<f64>,
+        b: Vec<f64>,
+        iterations: usize,
+    ) -> Vec<f64> {
+        let mut x = Matrix::new(b.len(), 1);
+        let mut r = b.to_row_vector();
+        let mut p = r.clone();
+
+        for _ in 0..iterations {
+            let r_t = r.t();
+            let a_p = vec_mul(p.get_elements_ref()).to_column_vector();
+            let alpha = (&r_t * &p)[0][0] / (p.t() * &a_p)[0][0];
+
+            let old_r = r.clone();
+            x = x + p.clone() * alpha;
+            r = r - a_p.clone() * alpha;
+
+            let beta = (r.t() * &r)[0][0] / (&r_t * &old_r)[0][0];
+            p = r.clone() + p.clone() * beta;
+        }
+
+        x.get_elements()
+    }
+}

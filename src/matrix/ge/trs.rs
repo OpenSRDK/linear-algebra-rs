18df8156f9591b9db025f1c3ad31d409017f4522
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-09-01 00:10:07 +0900

    docs: potrs

--- src/matrix/ge/trs.rs
+++ src/matrix/ge/trs.rs
@@ -4,81 +4,97 @@ use crate::number::c64;
 use lapack::{dgetrs, zgetrs};
 
 impl Matrix {
-  /// # Solve equation
-  /// with matrix decomposed by getrf
-  /// `Ax = b`
-  /// return x
-  pub fn getrs(&self, ipiv: &[i32], b: Matrix) -> Result<Matrix, MatrixError> {
-    let n = self.rows();
-    if n != self.cols() || n != b.rows {
-      return Err(MatrixError::DimensionMismatch);
+    #[cfg_attr(doc, katexit::katexit)]
+    /// # Solve equation
+    ///
+    /// with matrix decomposed by getrf
+    ///
+    /// $$
+    /// \mathbf{A} \mathbf{x} = \mathbf{b}
+    /// $$
+    ///
+    /// $$
+    /// \mathbf{x} = \mathbf{A}^{-1} \mathbf{b}
+    /// $$
+    pub fn getrs(&self, ipiv: &[i32], b: Matrix) -> Result<Matrix, MatrixError> {
+        let n = self.rows();
+        if n != self.cols() || n != b.rows {
+            return Err(MatrixError::DimensionMismatch);
+        }
+
+        let mut info = 0;
+
+        let n = n as i32;
+        let mut b = b;
+
+        unsafe {
+            dgetrs(
+                'N' as u8,
+                n,
+                b.cols as i32,
+                &self.elems,
+                n,
+                ipiv,
+                &mut b.elems,
+                n,
+                &mut info,
+            );
+        }
+
+        match info {
+            0 => Ok(b),
+            _ => Err(MatrixError::LapackRoutineError {
+                routine: "dgetrs".to_owned(),
+                info,
+            }),
+        }
     }
-
-    let mut info = 0;
-
-    let n = n as i32;
-    let mut b = b;
-
-    unsafe {
-      dgetrs(
-        'N' as u8,
-        n,
-        b.cols as i32,
-        &self.elems,
-        n,
-        ipiv,
-        &mut b.elems,
-        n,
-        &mut info,
-      );
-    }
-
-    match info {
-      0 => Ok(b),
-      _ => Err(MatrixError::LapackRoutineError {
-        routine: "dgetrs".to_owned(),
-        info,
-      }),
-    }
-  }
 }
 
 impl Matrix<c64> {
-  /// # Solve equation
-  /// with matrix decomposed by getrf
-  /// `Ax = b`
-  /// return xt
-  pub fn getrs(&self, ipiv: &[i32], bt: Matrix<c64>) -> Result<Matrix<c64>, MatrixError> {
-    let n = self.rows();
-    if n != self.cols() || n != bt.cols {
-      return Err(MatrixError::DimensionMismatch);
+    #[cfg_attr(doc, katexit::katexit)]
+    /// # Solve equation
+    ///
+    /// with matrix decomposed by getrf
+    ///
+    /// $$
+    /// \mathbf{A} \mathbf{x} = \mathbf{b}
+    /// $$
+    ///
+    /// $$
+    /// \mathbf{x} = \mathbf{A}^{-1} \mathbf{b}
+    /// $$
+    pub fn getrs(&self, ipiv: &[i32], bt: Matrix<c64>) -> Result<Matrix<c64>, MatrixError> {
+        let n = self.rows();
+        if n != self.cols() || n != bt.cols {
+            return Err(MatrixError::DimensionMismatch);
+        }
+
+        let mut info = 0;
+
+        let n = n as i32;
+        let mut bt = bt;
+
+        unsafe {
+            zgetrs(
+                'T' as u8,
+                n,
+                bt.rows as i32,
+                &self.elems,
+                n,
+                ipiv,
+                &mut bt.elems,
+                n,
+                &mut info,
+            );
+        }
+
+        match info {
+            0 => Ok(bt),
+            _ => Err(MatrixError::LapackRoutineError {
+                routine: "zgetrs".to_owned(),
+                info,
+            }),
+        }
     }
-
-    let mut info = 0;
-
-    let n = n as i32;
-    let mut bt = bt;
-
-    unsafe {
-      zgetrs(
-        'T' as u8,
-        n,
-        bt.rows as i32,
-        &self.elems,
-        n,
-        ipiv,
-        &mut bt.elems,
-        n,
-        &mut info,
-      );
-    }
-
-    match info {
-      0 => Ok(bt),
-      _ => Err(MatrixError::LapackRoutineError {
-        routine: "zgetrs".to_owned(),
-        info,
-      }),
-    }
-  }
 }
--- src/matrix/po/trs.rs
+++ src/matrix/po/trs.rs
@@ -4,103 +4,119 @@ use crate::number::c64;
 use lapack::{dpotrs, zpotrs};
 
 impl Matrix {
-  /// # Solve equation
-  /// with matrix decomposed by potrf
-  /// `Ax = b`
-  /// return xt
-  pub fn potrs(&self, b: Matrix) -> Result<Matrix, MatrixError> {
-    let n = self.rows();
-    if n != self.cols() || n != b.rows {
-      return Err(MatrixError::DimensionMismatch);
+    #[cfg_attr(doc, katexit::katexit)]
+    /// # Solve equation
+    ///
+    /// with matrix decomposed by potrf
+    ///
+    /// $$
+    /// \mathbf{A} \mathbf{x} = \mathbf{b}
+    /// $$
+    ///
+    /// $$
+    /// \mathbf{x} = \mathbf{A}^{-1} \mathbf{b}
+    /// $$
+    pub fn potrs(&self, b: Matrix) -> Result<Matrix, MatrixError> {
+        let n = self.rows();
+        if n != self.cols() || n != b.rows {
+            return Err(MatrixError::DimensionMismatch);
+        }
+
+        let mut info = 0;
+
+        let n = n as i32;
+        let mut b = b;
+
+        unsafe {
+            dpotrs(
+                'L' as u8,
+                n,
+                b.cols as i32,
+                &self.elems,
+                n,
+                &mut b.elems,
+                n,
+                &mut info,
+            );
+        }
+
+        match info {
+            0 => Ok(b),
+            _ => Err(MatrixError::LapackRoutineError {
+                routine: "dpotrs".to_owned(),
+                info,
+            }),
+        }
     }
-
-    let mut info = 0;
-
-    let n = n as i32;
-    let mut b = b;
-
-    unsafe {
-      dpotrs(
-        'L' as u8,
-        n,
-        b.cols as i32,
-        &self.elems,
-        n,
-        &mut b.elems,
-        n,
-        &mut info,
-      );
-    }
-
-    match info {
-      0 => Ok(b),
-      _ => Err(MatrixError::LapackRoutineError {
-        routine: "dpotrs".to_owned(),
-        info,
-      }),
-    }
-  }
 }
 
 impl Matrix<c64> {
-  /// # Solve equation
-  /// with matrix decomposed by potrf
-  /// `Ax = b`
-  /// return x_t
-  pub fn potrs(&self, b: Matrix<c64>) -> Result<Matrix<c64>, MatrixError> {
-    let n = self.rows();
-    if n != self.cols() || n != b.rows {
-      return Err(MatrixError::DimensionMismatch);
+    #[cfg_attr(doc, katexit::katexit)]
+    /// # Solve equation
+    ///
+    /// with matrix decomposed by potrf
+    ///
+    /// $$
+    /// \mathbf{A} \mathbf{x} = \mathbf{b}
+    /// $$
+    ///
+    /// $$
+    /// \mathbf{x} = \mathbf{A}^{-1} \mathbf{b}
+    /// $$
+    pub fn potrs(&self, b: Matrix<c64>) -> Result<Matrix<c64>, MatrixError> {
+        let n = self.rows();
+        if n != self.cols() || n != b.rows {
+            return Err(MatrixError::DimensionMismatch);
+        }
+
+        let mut info = 0;
+
+        let n = n as i32;
+        let mut b = b;
+
+        unsafe {
+            zpotrs(
+                'L' as u8,
+                n,
+                b.cols as i32,
+                &self.elems,
+                n,
+                &mut b.elems,
+                n,
+                &mut info,
+            );
+        }
+
+        match info {
+            0 => Ok(b),
+            _ => Err(MatrixError::LapackRoutineError {
+                routine: "zpotrs".to_owned(),
+                info,
+            }),
+        }
     }
-
-    let mut info = 0;
-
-    let n = n as i32;
-    let mut b = b;
-
-    unsafe {
-      zpotrs(
-        'L' as u8,
-        n,
-        b.cols as i32,
-        &self.elems,
-        n,
-        &mut b.elems,
-        n,
-        &mut info,
-      );
-    }
-
-    match info {
-      0 => Ok(b),
-      _ => Err(MatrixError::LapackRoutineError {
-        routine: "zpotrs".to_owned(),
-        info,
-      }),
-    }
-  }
 }
 
 #[cfg(test)]
 mod tests {
-  use crate::*;
-  #[test]
-  fn it_works() {
-    let a = mat![
-        2.0, 1.0;
-        1.0, 2.0
-    ];
-    let b = mat![
-        1.0, 3.0;
-        2.0, 4.0
-    ];
-    let l = a.potrf().unwrap();
-    let x_t = l.potrs(b).unwrap();
+    use crate::*;
+    #[test]
+    fn it_works() {
+        let a = mat![
+            2.0, 1.0;
+            1.0, 2.0
+        ];
+        let b = mat![
+            1.0, 3.0;
+            2.0, 4.0
+        ];
+        let l = a.potrf().unwrap();
+        let x_t = l.potrs(b).unwrap();
 
-    println!("{:#?}", x_t);
-    // assert_eq!(x_t[0][0], 0.0);
-    // assert_eq!(x_t[0][1], 1.0);
-    // assert_eq!(x_t[1][0], 5.0 / 3.0 - 1.0);
-    // assert_eq!(x_t[1][1], 5.0 / 3.0);
-  }
+        println!("{:#?}", x_t);
+        // assert_eq!(x_t[0][0], 0.0);
+        // assert_eq!(x_t[0][1], 1.0);
+        // assert_eq!(x_t[1][0], 5.0 / 3.0 - 1.0);
+        // assert_eq!(x_t[1][1], 5.0 / 3.0);
+    }
 }

a323f3038b09ae5ae33f45be70247eeefcf6c514
Rubens <91782701+tibetsou@users.noreply.github.com>
2021-10-11 21:30:46 +0900

    chore: オペレーターのテストを修正

--- src/matrix/operators/add.rs
+++ src/matrix/operators/add.rs
@@ -5,100 +5,98 @@ use std::ops::Add;
 
 fn add_scalar<T>(lhs: Matrix<T>, rhs: T) -> Matrix<T>
 where
-  T: Number,
+    T: Number,
 {
-  let mut lhs = lhs;
+    let mut lhs = lhs;
 
-  lhs
-    .elems
-    .par_iter_mut()
-    .map(|l| {
-      *l += rhs;
-    })
-    .collect::<Vec<_>>();
+    lhs.elems
+        .par_iter_mut()
+        .map(|l| {
+            *l += rhs;
+        })
+        .collect::<Vec<_>>();
 
-  lhs
+    lhs
 }
 
 impl<T> Add<T> for Matrix<T>
 where
-  T: Number,
+    T: Number,
 {
-  type Output = Matrix<T>;
+    type Output = Matrix<T>;
 
-  fn add(self, rhs: T) -> Self::Output {
-    add_scalar(self, rhs)
-  }
+    fn add(self, rhs: T) -> Self::Output {
+        add_scalar(self, rhs)
+    }
 }
 
 impl Add<Matrix> for f64 {
-  type Output = Matrix;
+    type Output = Matrix;
 
-  fn add(self, rhs: Matrix) -> Self::Output {
-    add_scalar(rhs, self)
-  }
+    fn add(self, rhs: Matrix) -> Self::Output {
+        add_scalar(rhs, self)
+    }
 }
 
 impl Add<Matrix<c64>> for c64 {
-  type Output = Matrix<c64>;
+    type Output = Matrix<c64>;
 
-  fn add(self, rhs: Matrix<c64>) -> Self::Output {
-    add_scalar(rhs, self)
-  }
+    fn add(self, rhs: Matrix<c64>) -> Self::Output {
+        add_scalar(rhs, self)
+    }
 }
 
 fn add<T>(lhs: Matrix<T>, rhs: &Matrix<T>) -> Matrix<T>
 where
-  T: Number,
+    T: Number,
 {
-  if !lhs.same_size(rhs) {
-    panic!("Dimension mismatch.")
-  }
-  let mut lhs = lhs;
+    if !lhs.same_size(rhs) {
+        panic!("Dimension mismatch.")
+    }
+    let mut lhs = lhs;
 
-  lhs
-    .elems
-    .par_iter_mut()
-    .zip(rhs.elems.par_iter())
-    .map(|(l, &r)| {
-      *l += r;
-    })
-    .collect::<Vec<_>>();
+    lhs.elems
+        .par_iter_mut()
+        .zip(rhs.elems.par_iter())
+        .map(|(l, &r)| {
+            *l += r;
+        })
+        .collect::<Vec<_>>();
 
-  lhs
+    lhs
 }
 
 impl<T> Add<Matrix<T>> for Matrix<T>
 where
-  T: Number,
+    T: Number,
 {
-  type Output = Matrix<T>;
+    type Output = Matrix<T>;
 
-  fn add(self, rhs: Matrix<T>) -> Self::Output {
-    add(self, &rhs)
-  }
+    fn add(self, rhs: Matrix<T>) -> Self::Output {
+        add(self, &rhs)
+    }
 }
 
 impl<T> Add<&Matrix<T>> for Matrix<T>
 where
-  T: Number,
+    T: Number,
 {
-  type Output = Matrix<T>;
+    type Output = Matrix<T>;
 
-  fn add(self, rhs: &Matrix<T>) -> Self::Output {
-    add(self, rhs)
-  }
+    fn add(self, rhs: &Matrix<T>) -> Self::Output {
+        add(self, rhs)
+    }
 }
 
 impl<T> Add<Matrix<T>> for &Matrix<T>
 where
-  T: Number,
+    T: Number,
 {
-  type Output = Matrix<T>;
+    type Output = Matrix<T>;
 
-  fn add(self, rhs: Matrix<T>) -> Self::Output {
-    add(rhs, self)
-  }
+    fn add(self, rhs: Matrix<T>) -> Self::Output {
+        add(rhs, self)
+    }
 }
 
 #[cfg(test)]

7e0c1ec30b4d29f63d0d19b72111689db1bcd318
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2021-10-12 21:55:42 +0900

    fix: test

--- src/matrix/sp/mul_rhs.rs
+++ src/matrix/sp/mul_rhs.rs
@@ -4,83 +4,83 @@ use std::ops::Mul;
 
 fn mul<T>(slf: &SparseMatrix<T>, rhs: &Matrix<T>) -> Matrix<T>
 where
-  T: Number,
+    T: Number,
 {
-  if slf.cols != rhs.rows {
-    panic!("Dimension mismatch.");
-  }
-  let mut new_matrix = Matrix::new(slf.rows, rhs.cols);
-
-  for (&(i, j), &s) in slf.elems.iter() {
-    for k in 0..rhs.cols {
-      new_matrix[i][k] += s * rhs[j][k];
+    if slf.cols != rhs.rows {
+        panic!("Dimension mismatch.");
     }
-  }
+    let mut new_matrix = Matrix::new(slf.rows, rhs.cols);
 
-  new_matrix
+    for (&(i, j), &s) in slf.elems.iter() {
+        for k in 0..rhs.cols {
+            new_matrix[(k, i)] += s * rhs[(j, k)];
+        }
+    }
+
+    new_matrix
 }
 
 impl<T> Mul<Matrix<T>> for SparseMatrix<T>
 where
-  T: Number,
+    T: Number,
 {
-  type Output = Matrix<T>;
+    type Output = Matrix<T>;
 
-  fn mul(self, rhs: Matrix<T>) -> Self::Output {
-    mul(&self, &rhs)
-  }
+    fn mul(self, rhs: Matrix<T>) -> Self::Output {
+        mul(&self, &rhs)
+    }
 }
 
 impl<T> Mul<&Matrix<T>> for SparseMatrix<T>
 where
-  T: Number,
+    T: Number,
 {
-  type Output = Matrix<T>;
+    type Output = Matrix<T>;
 
-  fn mul(self, rhs: &Matrix<T>) -> Self::Output {
-    mul(&self, rhs)
-  }
+    fn mul(self, rhs: &Matrix<T>) -> Self::Output {
+        mul(&self, rhs)
+    }
 }
 
 impl<T> Mul<Matrix<T>> for &SparseMatrix<T>
 where
-  T: Number,
+    T: Number,
 {
-  type Output = Matrix<T>;
+    type Output = Matrix<T>;
 
-  fn mul(self, rhs: Matrix<T>) -> Self::Output {
-    mul(self, &rhs)
-  }
+    fn mul(self, rhs: Matrix<T>) -> Self::Output {
+        mul(self, &rhs)
+    }
 }
 
 impl<T> Mul<&Matrix<T>> for &SparseMatrix<T>
 where
-  T: Number,
+    T: Number,
 {
-  type Output = Matrix<T>;
+    type Output = Matrix<T>;
 
-  fn mul(self, rhs: &Matrix<T>) -> Self::Output {
-    mul(self, rhs)
-  }
+    fn mul(self, rhs: &Matrix<T>) -> Self::Output {
+        mul(self, rhs)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-  use crate::*;
-  #[test]
-  fn it_works() {
-    let mut a = SparseMatrix::new(2, 3);
-    a[(0, 0)] = 1.0;
-    a[(0, 1)] = 2.0;
-    a[(1, 2)] = 3.0;
-    let b = mat![
-        1.0, 3.0;
-        2.0, 4.0;
-        3.0, 6.0
-    ];
-    let c = a * b;
+    use crate::*;
+    #[test]
+    fn it_works() {
+        let mut a = SparseMatrix::new(2, 3);
+        a[(0, 0)] = 1.0;
+        a[(0, 1)] = 2.0;
+        a[(1, 2)] = 3.0;
+        let b = mat![
+            1.0, 3.0;
+            2.0, 4.0;
+            3.0, 6.0
+        ];
+        let c = a * b;
 
-    assert_eq!(c[0][0], 5.0);
-    assert_eq!(c[1][1], 18.0);
-  }
+        assert_eq!(c[(0, 0)], 5.0);
+        assert_eq!(c[(1, 1)], 18.0);
+    }
 }

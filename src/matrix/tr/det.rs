5f3f903655a948a7b5cba8fa62b4a312fb876841
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-10-14 16:42:57 +0900

    feat: test-trdet

--- src/matrix/tr/det.rs
+++ src/matrix/tr/det.rs
@@ -1,13 +1,28 @@
 use crate::{matrix::Matrix, number::Number};
+use rayon::prelude::*;
 
 impl<T> Matrix<T>
 where
-  T: Number,
+    T: Number,
 {
-  /// # Determinant
-  /// for triangle matrix
-  /// To apply this method to none triangle matrix, use LU decomposition or Cholesky decomposition.
-  pub fn trdet(&self) -> T {
-    (0..self.rows).into_iter().map(|i| self[i][i]).product()
-  }
+    /// # Determinant
+    /// for triangle matrix
+    /// To apply this method to none triangle matrix, use LU decomposition or Cholesky decomposition.
+    pub fn trdet(&self) -> T {
+        (0..self.rows).into_par_iter().map(|i| self[i][i]).product()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+    #[test]
+    fn it_works() {
+        let a = mat!(
+            1.0, 0.0;
+            3.0, 4.0
+        );
+        let det = a.trdet();
+        assert_eq!(det, 4.0);
+    }
 }

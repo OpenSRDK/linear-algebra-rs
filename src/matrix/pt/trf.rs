9a496c65d434da478fc31ff55b248cd3154a36b6
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-10-14 18:35:09 +0900

    feat: zpttrf,zpttrs

--- src/matrix/pt/trf.rs
+++ src/matrix/pt/trf.rs
@@ -1,13 +1,18 @@
-use crate::matrix::MatrixError;
+use crate::matrix::*;
 use crate::DiagonalMatrix;
 use crate::{bd::BidiagonalMatrix, matrix::st::SymmetricTridiagonalMatrix};
 use lapack::dpttrf;
+use lapack::zpttrf;
 
-impl SymmetricTridiagonalMatrix<f64> {
+pub struct PTTRF<T = f64>(pub BidiagonalMatrix<T>, pub DiagonalMatrix)
+where
+    T: Number;
+
+impl SymmetricTridiagonalMatrix {
     /// # Cholesky decomposition
     /// for tridiagonal matrix
     /// `T = L * D * L^T`
-    pub fn pttrf(self) -> Result<(BidiagonalMatrix, DiagonalMatrix<f64>), MatrixError> {
+    pub fn pttrf(self) -> Result<PTTRF, MatrixError> {
         let (mut d, mut e) = self.eject();
         let n = d.len() as i32;
         let mut info = 0;
@@ -24,6 +29,32 @@ impl SymmetricTridiagonalMatrix<f64> {
         let bd = BidiagonalMatrix::new(vec![1.0; n as usize], e)?;
         let d = DiagonalMatrix::new(d);
 
-        Ok((bd, d))
+        Ok(PTTRF(bd, d))
+    }
+}
+
+impl SymmetricTridiagonalMatrix<c64> {
+    /// # Cholesky decomposition
+    /// for tridiagonal matrix
+    /// `T = L * D * L^T`
+    pub fn pttrf(self) -> Result<PTTRF<c64>, MatrixError> {
+        let (d, mut e) = self.eject();
+        let mut d = d.into_par_iter().map(|di| di.re).collect::<Vec<_>>();
+        let n = d.len() as i32;
+        let mut info = 0;
+
+        unsafe { zpttrf(n, &mut d, &mut e, &mut info) }
+
+        if info != 0 {
+            return Err(MatrixError::LapackRoutineError {
+                routine: "zpttrf".to_owned(),
+                info,
+            });
+        }
+
+        let bd = BidiagonalMatrix::<c64>::new(vec![c64::one(); n as usize], e)?;
+        let d = DiagonalMatrix::new(d);
+
+        Ok(PTTRF::<c64>(bd, d))
     }
 }
--- src/matrix/st/evd.rs
+++ src/matrix/st/evd.rs
@@ -2,43 +2,43 @@ use super::SymmetricTridiagonalMatrix;
 use crate::matrix::*;
 use lapack::dstevd;
 
-impl SymmetricTridiagonalMatrix<f64> {
-  /// # Eigen decomposition
-  /// return (lambda, pt)
-  pub fn stevd(self) -> Result<(Vec<f64>, Matrix), MatrixError> {
-    let (mut d, mut e) = self.eject();
-    let n = d.len();
-    let mut z = Matrix::new(n, n);
-    let lwork = 1.max(1 + 4 * n + n.pow(2));
-    let mut work = vec![0.0; lwork];
-    let liwork = 1.max(3 + 5 * n);
-    let mut iwork = vec![0; liwork];
-    let mut info = 0;
+impl SymmetricTridiagonalMatrix {
+    /// # Eigen decomposition
+    /// return (lambda, pt)
+    pub fn stevd(self) -> Result<(Vec<f64>, Matrix), MatrixError> {
+        let (mut d, mut e) = self.eject();
+        let n = d.len();
+        let mut z = Matrix::new(n, n);
+        let lwork = 1.max(1 + 4 * n + n.pow(2));
+        let mut work = vec![0.0; lwork];
+        let liwork = 1.max(3 + 5 * n);
+        let mut iwork = vec![0; liwork];
+        let mut info = 0;
 
-    let n = n as i32;
+        let n = n as i32;
 
-    unsafe {
-      dstevd(
-        'V' as u8,
-        n,
-        &mut d,
-        &mut e,
-        &mut z.elems,
-        n,
-        &mut work,
-        lwork as i32,
-        &mut iwork,
-        liwork as i32,
-        &mut info,
-      )
+        unsafe {
+            dstevd(
+                'V' as u8,
+                n,
+                &mut d,
+                &mut e,
+                &mut z.elems,
+                n,
+                &mut work,
+                lwork as i32,
+                &mut iwork,
+                liwork as i32,
+                &mut info,
+            )
+        }
+
+        match info {
+            0 => Ok((d, z)),
+            _ => Err(MatrixError::LapackRoutineError {
+                routine: "dstev".to_owned(),
+                info,
+            }),
+        }
     }
-
-    match info {
-      0 => Ok((d, z)),
-      _ => Err(MatrixError::LapackRoutineError {
-        routine: "dstev".to_owned(),
-        info,
-      }),
-    }
-  }
 }

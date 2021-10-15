389c1cf3a52d7dbdf77282df7a397c1de33d69d7
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-10-15 13:14:59 +0900

    feat: gttrf

--- src/matrix/gt/trs.rs
+++ src/matrix/gt/trs.rs
@@ -1,14 +1,16 @@
-use crate::{matrix::*, BidiagonalMatrix};
+use super::trf::GTTRF;
+use crate::matrix::*;
 use lapack::dgttrs;
+use lapack::zgttrs;
 
-impl BidiagonalMatrix<f64> {
+impl GTTRF {
     /// # Solve equation
     /// with matrix decomposed by gttrf
     /// `Ax = b`
     /// return x
-    pub fn gttrs(&self, u: &[Vec<f64>; 3], ipiv: &[i32], b: Matrix) -> Result<Matrix, MatrixError> {
-        let e = self.e();
-        let n = self.d().len() as i32;
+    pub fn gttrs(&self, b: Matrix) -> Result<Matrix, MatrixError> {
+        let GTTRF(dl, [d, du, du2], ipiv) = self;
+        let n = d.len() as i32;
         let mut b = b;
         let mut info = 0;
 
@@ -17,10 +19,10 @@ impl BidiagonalMatrix<f64> {
                 'N' as u8,
                 n,
                 b.cols as i32,
-                &e,
-                &u[0],
-                &u[1],
-                &u[2],
+                dl,
+                d,
+                du,
+                du2,
                 ipiv,
                 &mut b.elems,
                 n,
@@ -38,6 +40,43 @@ impl BidiagonalMatrix<f64> {
     }
 }
 
+impl GTTRF<c64> {
+    /// # Solve equation
+    /// with matrix decomposed by gttrf
+    /// `Ax = b`
+    /// return x
+    pub fn gttrs(&self, b: Matrix<c64>) -> Result<Matrix<c64>, MatrixError> {
+        let GTTRF::<c64>(dl, [d, du, du2], ipiv) = self;
+        let n = d.len() as i32;
+        let mut b = b;
+        let mut info = 0;
+
+        unsafe {
+            zgttrs(
+                'N' as u8,
+                n,
+                b.cols as i32,
+                dl,
+                d,
+                du,
+                du2,
+                ipiv,
+                &mut b.elems,
+                n,
+                &mut info,
+            )
+        }
+
+        match info {
+            0 => Ok(b),
+            _ => Err(MatrixError::LapackRoutineError {
+                routine: "zgttrs".to_owned(),
+                info,
+            }),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::*;
@@ -49,8 +88,8 @@ mod tests {
             4.0;
             2.0
         );
-        let result = a.clone().gttrf().unwrap();
-        let x = result.0.gttrs(&result.1, &result.2, b).unwrap();
+        let result = a.gttrf().unwrap();
+        let x = result.gttrs(b).unwrap();
         let ans = mat!(
             2.0;
             1.0;

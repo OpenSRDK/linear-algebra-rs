e691aa95500e5394fc4a1fcef85058923584f44a
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2021-10-15 23:21:47 +0900

    feat: par bd mod

--- src/matrix/bd/mod.rs
+++ src/matrix/bd/mod.rs
@@ -1,4 +1,5 @@
 use crate::{matrix::*, number::Number};
+use rayon::prelude::*;
 
 #[derive(Clone, Debug, Default, Hash)]
 pub struct BidiagonalMatrix<T = f64>
@@ -40,19 +41,16 @@ where
     pub fn mat(&self, upper: bool) -> Matrix<T> {
         let n = self.d.len();
         let mut mat = Matrix::new(n, n);
-
-        for i in 0..n {
-            mat[i][i] = self.d[i]
-        }
+        (0..n).into_par_iter().for_each(|i| mat[i][i] = self.d[i]);
 
         if upper {
-            for i in 0..n - 1 {
-                mat[i + 1][i] = self.e[i];
-            }
+            (0..n - 1)
+                .into_par_iter()
+                .for_each(|i| mat[i + 1][i] = self.e[i]);
         } else {
-            for i in 0..n - 1 {
-                mat[i][i + 1] = self.e[i];
-            }
+            (0..n - 1)
+                .into_par_iter()
+                .for_each(|i| mat[i][i + 1] = self.e[i]);
         }
 
         mat

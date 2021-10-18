fad57a8e9cdc1a0d8ff9c070ef3ae57ceaa7a087
Rubens <91782701+tibetsou@users.noreply.github.com>
2021-10-18 22:21:22 +0900

    feat packed作成！

--- src/matrix/sp_hp/mod.rs
+++ src/matrix/sp_hp/mod.rs
@@ -1,1 +1,44 @@
+use crate::number::Number;
+use crate::MatrixError;
+
 pub mod pp;
+
+pub struct SymmetricPackedMatrix<T = f64>
+where
+    T: Number,
+{
+    dim: usize,
+    elems: Vec<T>,
+}
+
+impl<T> SymmetricPackedMatrix<T>
+where
+    T: Number,
+{
+    pub fn new(dim: usize) -> Self {
+        Self {
+            dim,
+            elems: vec![T::default(); dim * (dim + 1) / 2],
+        }
+    }
+
+    pub fn from(dim: usize, elems: Vec<T>) -> Result<Self, MatrixError> {
+        if elems.len() != dim * (dim + 1) / 2 {
+            return Err(MatrixError::DimensionMismatch);
+        }
+
+        Ok(Self { dim, elems })
+    }
+
+    pub fn dim(&self) -> usize {
+        self.dim
+    }
+
+    pub fn vec(self) -> Vec<T> {
+        self.elems
+    }
+
+    pub fn slice(&self) -> &[T] {
+        &self.elems
+    }
+}

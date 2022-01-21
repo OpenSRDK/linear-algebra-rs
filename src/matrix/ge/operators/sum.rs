0b68f69e23e8e549e22c9483d1bfe321a19d683b
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2022-01-21 15:52:00 +0900

    feat: wip

--- src/matrix/ge/operators/sum.rs
+++ src/matrix/ge/operators/sum.rs
@@ -1,11 +1,27 @@
 use crate::{Matrix, Number};
 use std::iter::Sum;
 
-impl<T> Sum for Matrix<T>
+impl<T> Sum<Matrix<T>> for Matrix<T>
 where
     T: Number,
 {
     fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
         iter.reduce(|a, b| a + b).unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+    #[test]
+    fn it_works() {
+        let a = [mat!(
+            1.0, 2.0;
+            3.0, 4.0
+        ); 3]
+            .into_iter()
+            .sum::<Matrix>();
+
+        assert_eq!(a[(0, 0)], 3.0);
     }
 }

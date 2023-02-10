51fdfe57377cf07b01cfd5caf8ba894368fa2f23
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2023-02-10 12:06:45 +0800

    feat: inner prod

--- src/tensor/sparse/operations/prod.rs
+++ src/tensor/sparse/operations/prod.rs
@@ -9,7 +9,7 @@ pub trait TInnerProd<T>
 where
     T: Number,
 {
-    fn inner_prod(self, rank_combinations: &[Vec<Option<String>>]) -> SparseTensor<T>;
+    fn inner_prod(self, rank_combinations: &[HashMap<usize, String>]) -> SparseTensor<T>;
 }
 
 impl<I, T> TInnerProd<T> for I
@@ -17,24 +17,19 @@ where
     I: Iterator<Item = SparseTensor<T>>,
     T: Number,
 {
-    fn inner_prod(self, rank_combinations: &[Vec<Option<String>>]) -> SparseTensor<T> {
+    fn inner_prod(self, rank_combinations: &[HashMap<usize, String>]) -> SparseTensor<T> {
         let tensors = self.collect::<Vec<_>>();
         let max_rank = tensors.iter().map(|t| t.rank()).max().unwrap();
         let mut new_dims = vec![1; max_rank];
-        let mut identifier = HashMap::<String, (usize, usize)>::new();
 
         for (i, t) in tensors.iter().enumerate() {
             for (j, &dim) in t.dims.iter().enumerate() {
-                if rank_combinations[i][j].is_none() && dim > 1 {
+                if rank_combinations[i].get(&j).is_none() && dim > 1 {
                     if new_dims[j] == 1 {
                         new_dims[j] = dim;
                     } else {
                         panic!("The tensor whose a rank that is not aggregated and has a dimension greater than 1 can't be included.")
                     }
-                }
-
-                if let Some(id) = &rank_combinations[i][j] {
-                    identifier.insert(id.clone(), (i, j));
                 }
             }
         }
@@ -50,11 +45,12 @@ where
     T: Number,
 {
     pub fn inner_prod(self, rhs: Self, rank_pairs: &[(usize, usize)]) -> Self {
-        let mut rank_combinations = vec![vec![None; self.rank()], vec![None; rhs.rank()]];
+        let mut rank_combinations = vec![HashMap::new(); 2];
         for (i, rank_pair) in rank_pairs.iter().enumerate() {
-            rank_combinations[0][rank_pair.0] = Some(format!("id{}", i));
-            rank_combinations[1][rank_pair.1] = Some(format!("id{}", i));
+            rank_combinations[0].insert(rank_pair.0, i.to_string());
+            rank_combinations[1].insert(rank_pair.1, i.to_string());
         }
+
         vec![self, rhs].into_iter().inner_prod(&rank_combinations)
     }
 }

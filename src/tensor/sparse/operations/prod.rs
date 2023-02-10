2e445c52eb727c30cdb21721818901009b2e386c
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2023-02-10 10:59:06 +0800

    feat: inner prod

--- src/tensor/sparse/operations/prod.rs
+++ src/tensor/sparse/operations/prod.rs
@@ -1,29 +1,46 @@
+use std::collections::HashMap;
+
 use crate::tensor::Tensor;
 use crate::TensorError;
 use crate::{sparse::SparseTensor, Number};
 use rayon::prelude::*;
 
-impl<T> SparseTensor<T>
+pub trait TInnerProd<T>
 where
     T: Number,
 {
-    pub fn inner_prod(&self, rhs: &Self, rank_pairs: &[(usize, usize)]) -> Self {
-        for &rank_pair in rank_pairs {
-            if self.dim(rank_pair.0) != rhs.dim(rank_pair.1) {
-                panic!("Dimension mismatch.")
+    fn inner_prod(self, rank_combinations: &[Vec<Option<String>>]) -> SparseTensor<T>;
+}
+
+impl<I, T> TInnerProd<T> for I
+where
+    I: Iterator<Item = SparseTensor<T>>,
+    T: Number,
+{
+    fn inner_prod(self, rank_combinations: &[Vec<Option<String>>]) -> SparseTensor<T> {
+        let tensors = self.collect::<Vec<_>>();
+        let max_rank = tensors.iter().map(|t| t.rank()).max().unwrap();
+        let mut new_dims = vec![1; max_rank];
+        let mut identifier = HashMap::<String, (usize, usize)>::new();
+
+        for (i, t) in tensors.iter().enumerate() {
+            for (j, &dim) in t.dims.iter().enumerate() {
+                if rank_combinations[i][j].is_none() && dim > 1 {
+                    if new_dims[j] == 1 {
+                        new_dims[j] = dim;
+                    } else {
+                        panic!("The tensor whose a rank that is not aggregated and has a dimension greater than 1 can't be included.")
+                    }
+                }
+
+                if let Some(id) = &rank_combinations[i][j] {
+                    identifier.insert(id.clone(), (i, j));
+                }
             }
         }
 
-        let mut new_dims1 = self.dims.clone();
-        let mut new_dims2 = rhs.dims.clone();
-        rank_pairs.iter().for_each(|&rank_pair| {
-            new_dims1[rank_pair.0] = 1;
-            new_dims2[rank_pair.1] = 1;
-        });
+        let mut result = SparseTensor::<T>::new(new_dims);
 
-        // Convert to matrix format to calculate rapidly
-        for &rank_pair in rank_pairs {}
-
-        todo!();
+        result
     }
 }

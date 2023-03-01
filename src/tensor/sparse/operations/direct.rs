0f6377c615021524172db9a79a44eb9f9f52514a
Ryo0731 <95175659+Ryo0731@users.noreply.github.com>
2023-03-01 23:16:10 +0900

    feat: direct_product

--- src/tensor/sparse/operations/direct.rs
+++ src/tensor/sparse/operations/direct.rs
@@ -19,8 +19,6 @@ where
     fn direct_product(self) -> SparseTensor<T> {
         let terms = self.collect::<Vec<_>>();
         let rhs_size = &terms[terms.len() - 1].sizes;
-        println!("size0:{:?}", rhs_size);
-
         let new_sizes = terms.iter().fold(vec![], |mut acc, &next| {
             if acc.len() < next.sizes.len() {
                 for i in 0..acc.len() {
@@ -62,39 +60,23 @@ where
             )
             .into_iter()
             .map(|combination| {
-                // println!("{:?}", combination);
                 combination.into_iter().fold(
                     (Vec::<usize>::new(), T::default()),
                     |(mut accum_indices, mut accum_value), (term_index, indices)| {
-                        // println!(
-                        //     "accum_indices:{:?}, accum_value{:?}",
-                        //     accum_indices, accum_value
-                        // );
-                        // println!("term_index{:?}", term_index);
-
                         if accum_indices.is_empty() {
                             return (indices.clone(), terms[term_index].elem(&indices).clone());
                         }
-                        println!("accum_indices1:{:?}", accum_indices);
-                        println!("indices:{:?}", indices);
-                        // println!("term_index:{:?}", term_index);
 
                         if accum_indices.len() < indices.len() {
                             for i in 0..accum_indices.len() {
-                                accum_indices[i] = (accum_indices[i] + 1) * (indices[i] + 1) - 1;
+                                accum_indices[i] = accum_indices[i] * rhs_size[i] + indices[i];
                             }
                             accum_indices.extend(indices[accum_indices.len()..].iter());
                         } else {
                             for i in 0..indices.len() {
                                 accum_indices[i] = accum_indices[i] * rhs_size[i] + indices[i];
-                                // if accum_indices[i] == 0 {
-                                //     accum_indices[i] = 1;
-                                // }
                             }
                         }
-                        println!("accum_indices2:{:?}", accum_indices);
-                        println!();
-
                         accum_value *= terms[term_index].elem(&indices).clone();
 
                         (accum_indices, accum_value)
@@ -154,5 +136,70 @@ mod tests {
         c[&[3, 3]] = 32.0;
 
         assert_eq!(a.direct(&b), c);
+    }
+
+    #[test]
+    fn direct_product_two() {
+        let mut a = SparseTensor::<f64>::new(vec![2, 2]);
+        a[&[0, 0]] = 1.0;
+        a[&[0, 1]] = 2.0;
+        a[&[1, 0]] = 3.0;
+        a[&[1, 1]] = 4.0;
+
+        let mut b = SparseTensor::<f64>::new(vec![3, 3]);
+        b[&[0, 0]] = 5.0;
+        b[&[0, 1]] = 6.0;
+        b[&[1, 0]] = 7.0;
+        b[&[1, 1]] = 8.0;
+
+        let mut c = a.direct(&b);
+        println!("{:?}", c);
+    }
+    #[test]
+    fn direct_three_dimensional() {
+        let mut a = SparseTensor::<f64>::new(vec![2, 2, 2]);
+        a[&[0, 0, 0]] = 1.0;
+        a[&[0, 0, 1]] = 2.0;
+        a[&[0, 1, 0]] = 3.0;
+        a[&[0, 1, 1]] = 4.0;
+        a[&[1, 0, 0]] = 5.0;
+        a[&[1, 0, 1]] = 6.0;
+        a[&[1, 1, 0]] = 7.0;
+        a[&[1, 1, 1]] = 8.0;
+
+        let mut b = SparseTensor::<f64>::new(vec![2, 2, 2]);
+        b[&[0, 0, 0]] = 9.0;
+        b[&[0, 0, 1]] = 10.0;
+        b[&[0, 1, 0]] = 11.0;
+        b[&[0, 1, 1]] = 12.0;
+        b[&[1, 0, 0]] = 13.0;
+        b[&[1, 0, 1]] = 14.0;
+        b[&[1, 1, 0]] = 15.0;
+        b[&[1, 1, 1]] = 16.0;
+
+        let c = a.direct(&b);
+        println!("{:?}", c);
+
+        assert_eq!(c[&[0, 0, 0]], 9.0);
+
+        assert_eq!(c[&[0, 2, 0]], 27.0);
+        assert_eq!(c[&[0, 3, 1]], 36.0);
+
+        assert_eq!(c[&[0, 2, 2]], 36.0);
+        assert_eq!(c[&[0, 2, 3]], 40.0);
+        assert_eq!(c[&[0, 3, 2]], 44.0);
+        assert_eq!(c[&[0, 3, 3]], 48.0);
+
+        assert_eq!(c[&[1, 0, 0]], 13.0);
+        assert_eq!(c[&[1, 2, 3]], 56.0);
+        assert_eq!(c[&[1, 3, 3]], 64.0);
+
+        assert_eq!(c[&[2, 0, 0]], 45.0);
+        assert_eq!(c[&[2, 2, 2]], 72.0);
+        assert_eq!(c[&[2, 3, 3]], 96.0);
+
+        assert_eq!(c[&[3, 0, 0]], 65.0);
+        assert_eq!(c[&[3, 2, 2]], 104.0);
+        assert_eq!(c[&[3, 3, 3]], 128.0);
     }
 }
